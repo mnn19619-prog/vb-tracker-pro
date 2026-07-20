5b2bc32798a8d849087f05aea808be1bfdac6198
mnn19619-prog <mnn19619@gmail.com>
2026-07-19 22:52:25 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'vb-pro-v1';
+const URLS = ['/', '/manifest.json', '/icon.svg',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => Promise.allSettled(URLS.map(u => c.add(new Request(u,{mode:'cors'}))))));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  clients.claim();
+});
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
